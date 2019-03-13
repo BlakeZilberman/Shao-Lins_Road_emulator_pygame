a4d6f56f470be3e16c333cf63b6e76ece94601fe
BlakeZilberman <46869712+BlakeZilberman@users.noreply.github.com>
2019-03-13 18:50:23 +0000

    Add files via upload

--- Setup.py
+++ Setup.py
@@ -7,6 +7,8 @@ DISPLAYSURF = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Shao Lin's Road")
 heroImg = pygame.image.load('Assets/hero.png')
 heroImg = pygame.transform.scale(heroImg, (200, 200))
+herokickImg = pygame.image.load('Assets/herokick.png')
+herokickImg = pygame.transform.scale(herokickImg, (200, 200))
 bg = pygame.image.load("Assets/background.png")
 bg = pygame.transform.scale(bg, (800, 800))
 fpsClock = pygame.time.Clock()
@@ -18,19 +20,23 @@ enemyX = 400
 enemyY = 400
 enemyImg = pygame.image.load('Assets/enemy.png')
 enemyImg = pygame.transform.scale(heroImg, (200, 200))
+kicking = False
+
 
 
 while True: # main game loop
 
-    DISPLAYSURF.blit(bg, (0, 0))
-    DISPLAYSURF.blit(heroImg, (heroX, heroY))
-    DISPLAYSURF.blit(enemyImg, (enemyX, enemyY))
 
+    DISPLAYSURF.blit(bg, (0, 0))
+
+
+ 
 
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT] and heroX > -50: 
         heroX -= 10
+
 
     
 
@@ -38,9 +44,12 @@ while True: # main game loop
         heroX += 10
         
 
-    if keys[pygame.K_UP]:
-        heroY -= 74
-
+    if keys[pygame.K_SPACE]:
+        kicking = True
+        DISPLAYSURF.blit(herokickImg, (heroX, heroY))       
+    else:
+        kicking = False
+        DISPLAYSURF.blit(heroImg, (heroX, heroY))
 
 
     fpsClock.tick(FPS)
